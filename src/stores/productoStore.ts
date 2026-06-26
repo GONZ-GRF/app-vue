dadadd90c42b892db3226320fe957c2f28c14d34
Fabiana Gonzales Ramirez <fabiagonzales-sif@itpm.edu.bo>
2026-06-26 09:40:51 -0400

    Caso practico 1 Pinia Store

---
+++ src/stores/productoStore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export interface Producto {
+  id: number
+  nombre: string
+  precio: number
+  stock: number
+}
+
+export const useProductoStore = defineStore('producto', () => {
+
+  const productos = ref<Producto[]>([])
+
+  const totalInventario = computed(() =>
+    productos.value.reduce((total, producto) =>
+      total + producto.precio * producto.stock, 0)
+  )
+
+  function agregarProducto(producto: Producto) {
+    productos.value.push(producto)
+  }
+
+  return {
+    productos,
+    totalInventario,
+    agregarProducto
+  }
+})
